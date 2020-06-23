9ac69d5364ff7c85566d975e0404fe94dc1752c2
selvakumar2020 <selva.mrg2005@gmail.com>
2020-06-23 10:14:41 +0530

    first commit

---
+++ src/test/java/features/login.feature
@@ -0,0 +1,10 @@
+Feature: To check the Login page Validations
+Scenario: Login Page Validations
+Given User is on landing page
+When Enter the username "selva" and password "kumar" and click the submit button
+Then Login Home page displayed successfully
+
+Scenario: Login Page Validations2
+Given User is on landing page
+When Enter the username "gnana" and password "moorthy" and click the submit button
+Then Login Home page displayed successfully
---
+++ src/test/java/stepDefinitions/StepDefinition.java
@@ -0,0 +1,32 @@
+package stepDefinitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class StepDefinition {
+
+	
+
+		@Given("User is on landing page")
+		public void user_is_on_landing_page() {
+		    // Write code here that turns the phrase above into concrete actions
+		    System.out.println("Given statement Executed successfully");
+		}
+
+		@When("^Enter the username \"([^\"]*)\" and password \"([^\"]*)\" and click the submit button$")
+	    public void enter_the_username_something_and_password_something_and_click_the_submit_button(String username, String password) throws Throwable {
+	        System.out.println("When steps:"+username);
+	        System.out.println("When Steps passwrod:"+password);
+	    }
+		
+		@Then("Login Home page displayed successfully")
+		public void login_Home_page_displayed_successfully() {
+		    // Write code here that turns the phrase above into concrete actions
+		    System.out.println("Then Statement executed successfully");
+		}
+
+
+
+	
+}
